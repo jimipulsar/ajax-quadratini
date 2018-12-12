955e6e004e3a95f0299201ae465ca807089810e5
jimipulsar <jimipulsar@gmail.com>
2018-12-12 10:15:23 +0100

    Esercizio corretto e completato

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 
-var random = Math.ceil(Math.random() * 9);
-
 for (var i = 0; i < 35; i++) {
   document.getElementsByClassName('quadratini')[0].innerHTML += '<div class="quadrato"></div>'
 }
@@ -10,32 +8,29 @@ $(document).ready(function () {
     var clone = $('.quadrato').clone();
   }
 
-  $('.quadrato').click(function () {
-
+  $('.quadrato').one( "click", function() {
     var quadrato_cliccato = $(this);
-
     if (!quadrato_cliccato.hasClass('clicked')) {
       $.ajax({
         url: 'https://www.boolean.careers/api/random/int',
         method: 'GET',
         success: function (data) {
-          if (random <= 5)
-               {
-                 quadrato_cliccato.text(data.response);
-                 quadrato_cliccato.css("background-color", "yellow");
-               }
-               else {
-                       quadrato_cliccato.text(data.response);
-                       quadrato_cliccato.css("background-color", "green");
-
-                   }
+          var cpu_n = data.response;
+          var random = Math.floor(Math.random() * 10);
+          if (cpu_n <= 5)
+             {
+               quadrato_cliccato.text(cpu_n);
+               quadrato_cliccato.css("background-color", "yellow");
+             }
+             else if (cpu_n > 5){
+               quadrato_cliccato.text(data.response);
+               quadrato_cliccato.css("background-color", "green");
+          }
         },
         error: function (error) {
           alert(error);
         },
       });
     }
-
   });
-
 });
